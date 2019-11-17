42e04f67568c38b322a9102744e2d7e95b9e9add
UrielAbergel <myfliper2@gmail.com>
2019-11-17 22:08:55 +0200

    fix the print bug

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include "myMath.h"
 
@@ -9,12 +10,12 @@ int main() {
     printf("Error: You must to enter a number. Try again\n");
     else{
     printf("valid integer followed by enter key\n");
-    double ans  ;
+    float ans  ;
     ans = sub(add(Exp(x),Pow(x,3)),2) ;
     printf("The value of the function F(x) = e^x + x^3 -2 is %0.4f\n" ,ans);
-    ans = add(mul(x,3),mul(2,Pow(x,2)));;
+    ans = add(mul(x,3),mul(Pow(x,2),2));
     printf("The value of the function F(x) = 3x + 2x^2 is %0.4f\n" ,ans);
-    ans = sub(div(mul(4,Pow(x,3)),5),mul(2,x));
+    ans = sub(div(mul(Pow(x,3),4),5),mul(x,2));
     printf("The value of the function F(x) = (4x^3)/5 - 2*x is %0.4f\n" ,ans);
     }
    
